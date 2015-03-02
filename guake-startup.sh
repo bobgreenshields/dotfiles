b9a2dd1b267fc34379c5459ef0770f44921c8640
Bob Greenshields <bob@thegreenshields.com>
2015-03-02 09:27:13 +0000

    tighten up startup, remove scans tab

--- guake-startup.sh
+++ guake-startup.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 # start guake
 guake &
-
 sleep 2
-
 guake --rename-tab="Main" --execute-command="~/dotfiles/guake-tmux.sh" &
-
 sleep 2
-
 guake --new-tab=2 --rename-tab="Downloads" --execute-command="cd ~/Downloads" &
-
 sleep 2
-
-guake --new-tab=3 --rename-tab="Scans" --execute-command="echo type scn to switch to scans directory" &
-
-sleep 2
-
 guake --new-tab=3 --rename-tab="Dropbox" --execute-command="cd ~/Dropbox" &
-
 sleep 2
-
 guake --select-tab=0 &
+notify-send Guake "Is now configured" -i /usr/share/icons/hicolor/48x48/apps/guake.png
